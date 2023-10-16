1ccf5652b308fdececf4ed21b17504dedccc1da2
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-10-16 14:40:20 -0600

    Corregir errores

--- rotacion.c
+++ rotacion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <libjpeg.h>
-#include <libpng.h>
+#include <jpeglib.h>
+#include <png.h>
 
 void rotarJPEG(const char *input, const char *output) {
     // Código para rotar imágenes JPEG 
@@ -24,13 +24,17 @@ int main(int argc, char *argv[]) {
     int flag_convolucion_jpg = 0;
     int flag_convolucion_png = 0;
 
+    const char *input = "input.png";
+    const char *output = "output.png";
+
+
     if ((flag_rotacion_png == 1) && (flag_convolucion_png == 1)){
-        rotarpng(input, output);
+        rotarPNG(input, output);
 
     } if ((flag_rotacion_jpg == 1) && (flag_convolucion_jpg == 1)){
         rotarJPEG(input, output);
     } else {
-        printf(stderr, "Formato no compatible. Utilice 'jpeg' o 'png'.\n");
+        fprintf(stderr, "Formato no compatible. Utilice 'jpeg' o 'png'.\n");
         return 1;
     }
 
